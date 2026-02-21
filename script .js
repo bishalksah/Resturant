3714110332e78ec23928d90f36234d2646d3505e
vshalsah29 <vshalsah29@gmail.com>
2026-02-22 00:52:18 +0530

    regular update

---
+++ script .js
@@ -0,0 +1,25 @@
+<script>
+function toggleMenu(){
+    document.getElementById("nav-links").classList.toggle("active");
+}
+
+function scrollToMenu(){
+    document.getElementById("menu").scrollIntoView({behavior:"smooth"});
+}
+
+function filterMenu(category){
+    let items = document.querySelectorAll(".card");
+    let buttons = document.querySelectorAll(".filter-buttons button");
+
+    buttons.forEach(btn => btn.classList.remove("active"));
+    event.target.classList.add("active");
+
+    items.forEach(item => {
+        if(category === "all" || item.dataset.category === category){
+            item.style.display = "block";
+        } else {
+            item.style.display = "none";
+        }
+    });
+}
+</script>
